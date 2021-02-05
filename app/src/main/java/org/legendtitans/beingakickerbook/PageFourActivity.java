cb7c6ebbd9d1274dd4f864b37fd5d285e979b710
legend-patrick <Aly55a55@@>
2021-02-05 09:39:16 -0700

    Initial Upload

---
+++ app/src/main/java/org/legendtitans/beingakickerbook/PageFourActivity.java
@@ -0,0 +1,14 @@
+package org.legendtitans.beingakickerbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PageFourActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_four);
+    }
+}
